226cfc837e1a1b918207453a28ae276e615a4b1f
thurPCC <165666256+thurPCC@users.noreply.github.com>
2025-08-29 12:32:46 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'O HTML (Linguagem de Marcação de HiperTexto)' 
+    'Pergunta 1',
+    'Quantas peças de nave tem no GTA V?',
+    '50 Peças' 
 )
 
 criaCartao(
-    'Programação',
+    'Pergunta 2',
     'O que é CSS?',
     'O CSS é uma linguagem de estilização.'
 )
 
 criaCartao(
-    'Programação',
+    'Pergunta 3',
     'O que é JavaScript?',
     'O JavaScript é uma linguagem de programação.'    
 )
 
 criaCartao(
-    'Programação',
+    'Pergunta 4',
     'O que é Front-end?',
     ' O Front-end compreende a parte visual de sites e aplicações.'
 )
 
 criaCartao(
-    'Programação',
+    'Pergunta 5',
     'O que é Back-end?',
     'O Back-end compreende tudo aquilo que tem por trás de uma aplicação.'
 )
 
 criaCartao(
-    'Programação',
+    'Pergunta 6',
     'O que é Full Stack?',
     'A Full Stack developer é uma pessoa que trabalha tanto com Front-end quanto com Back-end.'   
 ) 
